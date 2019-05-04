14fc20c06c501db3623476ba5bbba44cb7b13b2a
Phu <vongocphu04@gmail.com>
2019-05-04 09:43:08 +0700

    change update user image

--- database/migrations/2019_05_01_144802_create_category_table.php
+++ database/migrations/2019_05_01_144802_create_category_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateOrderTable extends Migration
+class CreateCategoryTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,8 @@ class CreateOrderTable extends Migration
      */
     public function up()
     {
-        Schema::create('orders', function (Blueprint $table) {
-            $table->increments('id');
-            $table->timestamps();
+        Schema::table('categories', function (Blueprint $table) {
+            //
         });
     }
 
@@ -26,6 +25,8 @@ class CreateOrderTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('orders');
+        Schema::table('categories', function (Blueprint $table) {
+            //
+        });
     }
 }
